6442a9abb20a5b3d5086a9ce8202b000fb07c98e
Tiago Wanke Marques <tiagowanke@gmail.com>
2024-02-12 22:37:38 +0100

    Created stories file to show-more-list.directive.

---
+++ libs/show-more-list/src/lib/show-more-list.directive.stories.ts
@@ -0,0 +1,60 @@
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+import { ShowMoreListDirective } from './show-more-list.directive';
+import { Meta, StoryObj } from '@storybook/angular';
+
+@Component({
+  standalone: true,
+  imports: [CommonModule, ShowMoreListDirective],
+  selector: 'dummy',
+  template: `
+    <ul *showMoreList="minItems;showMoreText:'Mostrar Mais';showLessText:'Mostrar Menos'">
+      <li *ngFor="let item of list">{{item}}</li>
+    </ul>
+    <br><br>
+    <button (click)="toggleList()">Toggle list</button>
+  `
+})
+export class DummyComponent {
+
+  @Input()
+  listOne = [
+    'item 1',
+    'item 2',
+    'item 3',
+  ]
+
+  @Input()
+  listTwo = [
+    'item 1',
+    'item 2',
+    'item 3',
+    'item 4',
+    'item 5',
+  ]
+
+  @Input()
+  minItems!: number;
+
+  list = this.listTwo;
+
+  toggleList(): void {
+    this.list = this.list === this.listOne ? this.listTwo : this.listOne;
+  }
+}
+
+const meta: Meta<DummyComponent> = {
+  component: DummyComponent,
+  title: 'ShowMoreList',
+  // argTypes: {
+  //   minItems: { control: 'number' }
+  // }
+};
+export default meta;
+type Story = StoryObj<DummyComponent>;
+
+export const Primary: Story = {
+  args: {
+    minItems: 4
+  }
+}
