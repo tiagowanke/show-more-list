6d7e1efcb61ddebd99daed16dd4311341056754c
Tiago Wanke Marques <tiagowanke@gmail.com>
2024-02-12 22:36:21 +0100

    Added lcov as a report to jest configuration in show-more-list lib.

--- libs/show-more-list/jest.config.ts
+++ libs/show-more-list/jest.config.ts
@@ -4,6 +4,7 @@ export default {
   preset: '../../jest.preset.js',
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
   coverageDirectory: '../../coverage/libs/show-more-list',
+  coverageReporters: ['lcov'],
   transform: {
     '^.+\\.(ts|mjs|js|html)$': [
       'jest-preset-angular',
